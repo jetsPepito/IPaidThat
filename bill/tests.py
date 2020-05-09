ca934e11ec31fa4f9511b21f4fc5420baf329a70
Louis Le Gatt <louis.le-gatt@epita.fr>
2020-05-09 12:02:36 +0200

    Last one

--- bill/tests.py
+++ bill/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+from django.test import TestCase
+from bill.models import *
+from datetime import datetime
+from .utils import *
+
+class RetObjTest(TestCase):
+    def setUp(self):
+        dat = datetime.strptime("10/08/12", "%d/%m/%y")
+        RetObject.objects.create(email = "toto@gmail.com",
+                                 invoice_number="Project 1",
+                                 date= dat.date(),
+                                 client_name= "Titi",
+                                 total_ttc = "2000 €",
+                                 total_vat = "2500 €")
+
+    def test_emailandclient_name(self):
+        em = RetObject.objects.get(email="toto@gmail.com")
+        client_name = RetObject.objects.get(client_name="Titi")
+        self.assertEqual(em.email, 'toto@gmail.com')
+        self.assertEqual(client_name.client_name, 'Titi')
+
+class TestBack(TestCase):
+    def test_back(self):
+        back("legatt.poubelle@gmail.com", "indo")
+        list = RetObject.objects.filter(invoice_number="legatt.poubelle@gmail.com")
+        for el in list:
+            self.assertEqual(el.email, "legatt.poubelle@gmail.com")
--- bill/views.py
+++ bill/views.py
@@ -1,58 +1,9 @@
 from django.shortcuts import render
-import requests
-from bs4 import BeautifulSoup
 from bill.forms import *
 from .models import RetObject
-from datetime import date, datetime
+from .utils import back
 
 # Create your views here.
-from django.http import HttpResponse
-
-
-# THE AUTHENTIFICATION FUNCTION : login to the website
-def authentification(email, password, session):
-    headers = {
-        "accept": "application/json, text/javascript, */*; q=0.01",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "en-US,en;q=0.9,fr;q=0.8",
-        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "origin": "https://app.factomos.com",
-        "referer": "https://app.factomos.com/connexion",
-        "x-requested-with": "XMLHttpRequest",
-    }
-    url = "https://app.factomos.com/controllers/app-pro/login-ajax.php"
-    session.post(url, data="appAction=login&email=" + email + "&password=" + password, headers=headers)
-
-    # GET ALL INFORMATION OF THE HTML
-
-
-def get_factures_elements(facture, email):
-    soup = BeautifulSoup(facture.text, "html.parser")
-
-    invoice = soup.findAll("td", class_="ITEM-NUMBER")
-    date = soup.findAll("td", class_="ITEM-DATE")
-    client_name = soup.findAll("td", class_="ITEM-CLIENT")
-    total_ttc = soup.findAll("td", class_="ITEM-TOT-TTC")
-    total_tva = soup.findAll("td", class_="ITEM-TOT-TVA")
-
-    for i in range(len(invoice)):  # INSERT INTO THE SQL
-        date_t = datetime.strptime(date[i].get_text().rstrip(), "%d/%m/%y")
-        RetObject.objects.get_or_create(
-            email=email,
-            invoice_number=invoice[i].get_text().rstrip(),
-            date=date_t.date(),
-            client_name=client_name[i].get_text().rstrip(),
-            total_ttc=total_ttc[i].get_text().rstrip(),
-            total_vat=total_tva[i].get_text().rstrip()
-        )
-
-
-# THE MAIN FUNCTION
-def back(email, password):
-    session = requests.Session()
-    authentification(email, password, session)
-    factures = session.get("https://app.factomos.com/mes-factures")
-    get_factures_elements(factures, email)
 
 
 # THE VIEW
